c18e3f59d8d9257236e3ea54499c6026e891232c
Haoran Li <xeal3k@gmail.com>
2020-02-04 14:13:35 -0500

    add code

---
+++ Triangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+# -*- coding: utf-8 -*-
+
+def classifyTriangle(a,b,c):
+    """
+    This function returns a string with the type of triangle from three integer values
+    corresponding to the lengths of the three sides of the Triangle.
+    
+    return:
+        If all three sides are equal, return 'Equilateral'
+        If exactly one pair of sides are equal, return 'Isoceles'
+        If no pair of  sides are equal, return 'Scalene'
+        If not a valid triangle, then return 'NotATriangle'
+        If the sum of any two sides equals the squate of the third side, then return 'Right'
+    """
+    # verify that all 3 inputs are integers  
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not (isinstance(a,int) or (isinstance(a,float))):
+        return 'InvalidInput'
+    if not (isinstance(b,int) or (isinstance(b,float))):
+        return 'InvalidInput'
+    if not (isinstance(c,int) or (isinstance(c,float))):
+        return 'InvalidInput'
+                                                                                                                
+    
+    # require that the input values be >= 0 and <= 200
+    if a > 200 or b > 200 or c > 200:
+        return 'InvalidInput'
+        
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+      
+    # This information was not in the requirements spec but 
+    # is important for correctness
+    # the sum of any two sides must be strictly less than the third side
+    # of the specified shape is not a triangle
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+        return 'NotATriangle'
+        
+    # now we know that we have a valid triangle 
+    if a == b and b == c:
+        return 'Equilateral'
+    elif a == b or a == c or b == c:
+        return 'Isoceles'
+    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((c ** 2) + (b ** 2)) == (a ** 2):
+        return 'Right'
+    else:
+        return 'Scalene'
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
