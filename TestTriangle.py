1b151a1c4af8981b746a95ae5ed1f9553bd8fdbb
Haoran Li <xeal3k@gmail.com>
2020-02-04 14:19:17 -0500

    new test file

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: jrr
-@author: rk
-"""
+# In[5]:
+
 
 import unittest
 
@@ -17,16 +14,31 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
+    def testNotATriangle(self):
+        self.assertNotEqual(classifyTriangle(1,2,'1'),'Isoceles','1,2,1 should not be a triangle')
+        self.assertNotEqual(classifyTriangle(201, 201, 202),'Isoceles','201, 201, 202 should not be a triangle')
+        self.assertNotEqual(classifyTriangle(-1, -2, -2),'Isoceles','-1, -2, -2 should not be a triangle')
+        
+    def testRightTriangle(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
-    def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        
+    def testIsocelesTriangle(self):
+        self.assertEqual(classifyTriangle(1,2,2),'Isoceles','1,2,1 should be isoceles')
+        
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(2,3,4),'Scalene','2,3,4 should be scalene')
 
 if __name__ == '__main__':
     print('Running unit tests')
-    unittest.main()
+    unittest.main(argv=['first-arg-is-ignored'], exit=False, verbosity = 2)
+
+
+# In[ ]:
+
+
+
 
